e7ba31f3ae9923a9a5c386c70cd0b84e63323e82
hututu193 <116326894+hututu193@users.noreply.github.com>
2025-11-22 21:05:38 +0800

    login页面功能

---
+++ src/components/NotebookList.vue
@@ -0,0 +1,21 @@
+<!-- components/SimpleTest.vue -->
+<template>
+  <div>
+    <h1>简单测试页面</h1>
+    <p>当前时间: {{ currentTime }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SimpleTest',
+  data() {
+    return {
+      currentTime: new Date().toLocaleTimeString()
+    }
+  },
+  mounted() {
+    console.log('SimpleTest 组件已挂载')
+  }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+// router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+
+// 只有最核心的组件（如首页、登录页）使用同步导入
+import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/components/Login.vue'
+import Notebooks from '@/components/NotebookList.vue'
+import NoteDetail from '@/components/NoteDetail.vue'
+import TrashDetail from '@/components/TrashDetail.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'HelloWorld',
+        component: HelloWorld
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    // 其他所有组件都使用懒加载
+    {
+        path: '/notebooks',
+        name: 'Notebooks',
+        component: () => import('@/components/NotebookList.vue')
+    },
+    {
+        path: '/note/:noteId',
+        name: 'NoteDetail',
+        component: () => import('@/components/NoteDetail.vue')
+    },
+    {
+        path: '/trash/:noteId',
+        name: 'TrashDetail',
+        component: () => import('@/components/TrashDetail.vue')
+    },
+    // 可以添加加载状态
+    // {
+    //     path: '/loading',
+    //     name: 'Loading',
+    //     component: () => import('@/components/LoadingState.vue')
+    // }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router
